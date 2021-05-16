b9f7c9961c840f8c01899989950232625ac30e9f
cloworm <chloe.echikson@gmail.com>
2021-05-16 13:10:11 -0400

    added basic type checking derived from jsdoc

--- src/modules/my/screen/screen.js
+++ src/modules/my/screen/screen.js
@@ -1,8 +1,14 @@
 import { LightningElement, api } from 'lwc';
-
 export default class Screen extends LightningElement {
+  /**
+   * @type {number}
+   */
   @api value = 0;
 
+  /**
+   * Getter returns formatted value
+   * @return {string}
+   */
   get displayValue() {
     return this.value.toLocaleString();
   }
